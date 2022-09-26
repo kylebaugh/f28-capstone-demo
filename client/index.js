4f2d6e19b49078b0f07bd51bb12ad3149409e60e
Kyle Baugh <kylebaugh1@gmail.com>
2022-09-26 00:05:32 -0600

    display features in progress

--- client/index.js
+++ client/index.js
@@ -4,10 +4,43 @@ const baseURL = 'http://localhost:4567'
 // Step 1: Select Elements
 
 const addButton = document.querySelector('#addDrink')
+const showDrinks = document.querySelector('#drinkDisplay')
 
 // Step 2: Write functions
 
+const createDrinkCard = (drink) => {
+    const drinkCard = document.createElement('section')
+    drinkCard.classList.add('drink-card')
+
+    drinkCard.innerHTML = `
+        <img alt='drink image' src='${drink.picture}'>
+        <p>${drink.name}</p>
+        <p>${drink.flavor}</p>
+        <section>
+            <button>Down</button>
+            ${drink.likes}
+            <button>Up</button>
+        </section>
+        `
+
+    showDrinks.appendChild(drinkCard)
+}
+
+const getAllDrinks = () => {
+    axios.get(`${baseURL}/getDrinks`)
+        .then((res) => {
+            showDrinks.innerHTML = ''
+            let drinks = res.data
+            for(let i = 0; i < drinks.length; i++){
+                createDrinkCard(drinks[i])
+            }
+
+        })
+}
+
 const addDrink = () => {
+                showDrinks.innerHTML = ''
+
     let name = document.querySelector('#nameInput')
     let flavor = document.querySelector('#flavorInput')
     let image = document.querySelector('#imageInput')
@@ -21,6 +54,18 @@ const addDrink = () => {
     axios.post(`${baseURL}/addDrink`, bodyObj)
         .then((res) => {
             console.log(res.data)
+
+            showDrinks.innerHTML = ''
+
+            name.value = ''
+            flavor.value = ''
+            image.value = ''
+
+            let drinks = res.data
+            for(let i = 0; i < drinks.length; i++){
+                createDrinkCard(drinks[i])
+            }
+
         })
         .catch((err) => {
             console.log(err)
@@ -30,3 +75,6 @@ const addDrink = () => {
 // Step 3: Combine with event listener
 
 addButton.addEventListener('click', addDrink)
+
+
+getAllDrinks()
